a3f8f0d51ad67654d7b1f8788e1059cb53e34bbb
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-04-02 00:37:15 +0100

    Add api

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,18 +1,18 @@
-// /* eslint-disable react/jsx-no-target-blank */
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { RANDOM_GREETING } from '../redux/greeting';
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RANDOM_GREETING } from '../redux/greeting';
 
-// const Greetings = () => {
-//   const greetings = useSelector((state) => state.greeting);
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(RANDOM_GREETING());
-//   }, [dispatch]);
+const Greetings = () => {
+  const greetings = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(RANDOM_GREETING());
+  }, [dispatch]);
 
-//   return (
-//     <h1>{greetings.greeting}</h1>
-//   );
-// };
+  return (
+    <h1>{greetings.greeting}</h1>
+  );
+};
 
-// export default Greetings;
+export default Greetings;
