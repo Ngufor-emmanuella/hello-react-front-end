069ae20036a5e9e7c40b07fef6de927be544b299
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-04-02 01:47:44 +0100

    Add rails

--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -1,5 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 
 // implementation
-export const randomGreeting = 'greeting/RANDOM_GREETING';
+const randomGreeting = 'greeting/RANDOM_GREETING';
 export const baseurl = 'http://[::1]:3000/api/greetings';
+
+export default randomGreeting;
